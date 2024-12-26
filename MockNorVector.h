e3029a4ca6b171439eed8b2697860a3cf60b3b12
DavidCage31 <85827985+DavidCage31@users.noreply.github.com>
2024-12-26 13:52:35 +0300

    Update MockNorVector.h

--- MockNorVector.h
+++ MockNorVector.h
@@ -15,31 +15,31 @@ private:
 public:
     MockNorVector() {
         capacity = N = 0;
-		norVec = new norVec_Ty[N];
-		arr = nullptr;
+	norVec = new norVec_Ty[N];
+	arr = nullptr;
     }
     MockNorVector(const size_t size) {
         capacity = N = size;
-		norVec = new norVec_Ty[capacity];
-		arr = nullptr;
+	norVec = new norVec_Ty[capacity];
+	arr = nullptr;
     }
     MockNorVector(const MockNorVector<norVec_Ty>& other) {
-		capacity = other.capacity;
-		N = other.N;
-		norVec = new norVec_Ty[capacity];
-		arr = nullptr;
+	capacity = other.capacity;
+	N = other.N;
+	norVec = new norVec_Ty[capacity];
+	arr = nullptr;
 
-		for (int i = 0; i < N; i++)
-			norVec[i] = other.norVec[i];
+	for (int i = 0; i < N; i++)
+		norVec[i] = other.norVec[i];
 	}
-	MockNorVector(const std::vector<norVec_Ty>& vec) {
-		capacity = N = vec.size();
-		norVec = new norVec_Ty[capacity];
-		arr = nullptr;
+    MockNorVector(const std::vector<norVec_Ty>& vec) {
+	capacity = N = vec.size();
+	norVec = new norVec_Ty[capacity];
+	arr = nullptr;
 
-		for (size_t i = 0; i < N; i++)
-			norVec[i] = vec[i];
-	}
+	for (size_t i = 0; i < N; i++)
+		norVec[i] = vec[i];
+    }
 
     ~MockNorVector() {
         delete[] norVec;
@@ -47,12 +47,12 @@ public:
     }
 
     MOCK_METHOD1_T(push_back, void(const norVec_Ty& value));
-	MOCK_METHOD0_T(pop_back, norVec_Ty());
+    MOCK_METHOD0_T(pop_back, norVec_Ty());
     MOCK_METHOD1_T(make_size, void(const size_t size));
 
     MOCK_METHOD0_T(reverse, void());
 
-	MOCK_METHOD1_T(operatorIndex, norVec_Ty(const size_t index));
-	MOCK_METHOD0_T(size, size_t());
+    MOCK_METHOD1_T(operatorIndex, norVec_Ty(const size_t index));
+    MOCK_METHOD0_T(size, size_t());
 };
 
